07095d3e8d749729ce856e1dd6911ffda2c89fac
Jiajie "George" Chen <jiajiechen0319@hotmail.com>
2016-04-27 17:21:21 -0400

    Create ex2.py

---
+++ CSE-6240-Web-Search/hw2/part1/ex2.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+"""
+    Author: Jiajie Chen
+    Date: Apr 24, 2016
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from logisticRegression import logisticRegression
+
+os.chdir("/Users/jiajiechen/Desktop/CSE6240-HW2")
+
+data = pd.read_csv("./submit/ex2data1.txt", delimiter=",", header=None)
+X = data.values[:,:2]
+y = data.values[:,2]
+del(data)
+
+clf = logisticRegression(lambd=0,tol=1e-6,verbose=True)
+clf.fit(X,y)
+
+y_pred = clf.predict(X)
+print("train accuracy =", clf.accuracy(y_pred, y))
+
+# add decision boundary
+xx = np.linspace(X[:,0].min(), X[:,0].max())
+yy = -clf.theta[1]/clf.theta[2]*xx - clf.theta[0]/clf.theta[2]
+
+plt.figure()
+plt.scatter(X[:,0],X[:,1],c=y,s=40,alpha=0.8,cmap=plt.cm.Paired)
+
+plt.figure()
+plt.scatter(X[:,0],X[:,1],c=y,s=40,alpha=0.8,cmap=plt.cm.Paired)
+plt.plot(xx, yy, 'k-')
+plt.show()
